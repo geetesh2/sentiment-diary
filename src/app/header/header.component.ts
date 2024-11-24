b691635a00e5df3f9631ddb2da58b96fc92ce038
Geetesh <geeteshpandey2@gmail.com>
2024-11-24 19:02:53 +0530

    static chart prepared

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatButtonModule,MatToolbarModule],
+  imports: [MatButtonModule,MatToolbarModule,RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
--- src/app/trends/trends.component.ts
+++ src/app/trends/trends.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartConfiguration, ChartType } from 'chart.js';
+import {CategoryScale} from 'chart.js'; 
 
 @Component({
   selector: 'app-trends',
   standalone: true,
-  imports: [],
+  imports: [BaseChartDirective], // Import NgChartsModule
   templateUrl: './trends.component.html',
-  styleUrl: './trends.component.css'
+  styleUrls: ['./trends.component.css']
 })
 export class TrendsComponent {
+  
+  public lineChartData: ChartConfiguration<'line'>['data'] = {
+    labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    datasets: [
+      {
+        data: [70, 65, 75, 80, 60, 85, 90], // Example sentiment scores
+        label: 'Mood Score',
+        fill: true,
+        tension: 0.5,
+        borderColor: '#42A5F5',
+        backgroundColor: 'rgba(66, 165, 245, 0.3)',
+      },
+    ],
+  };
 
+  public lineChartOptions: ChartConfiguration<'line'>['options'] = {
+    responsive: true,
+    scales: {
+      x: {},
+      y: {
+        beginAtZero: true,
+        max: 100,
+      },
+    },
+  };
+
+  public lineChartType: ChartType = 'line';
 }
