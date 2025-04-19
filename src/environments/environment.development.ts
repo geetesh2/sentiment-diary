c2ce00fb62919d27e7aed80510581fdea33da327
Geetesh Pandey <94466254+geetesh2@users.noreply.github.com>
2025-04-19 15:38:35 +0530

    Delete src/environments/environment.development.ts

--- src/environments/environment.development.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-    firebaseApiKey: 'AIzaSyBZGaR-7DzhvI-JaoKTSRoQCaVB65HkH98',
-    firebaseDbUrl: 'https://sentiment-diary-197da-default-rtdb.asia-southeast1.firebasedatabase.app/',
-    sentimentApiUrl: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent',
-    sentimentApiKey: 'AIzaSyCTZa31NubwmPWcLlX2UPmCIzdMZ1yZbgY'
-  };
-  
